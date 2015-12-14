0d5b86b88336e45153733856bdba63ec2eee50b7
Andreas Hermann <a.v.hermann@gmail.com>
2015-12-14 18:41:11 +0800

    add time blending video filter for smoother time lapses

---
+++ src/VideoRendererMetadataDefinition.lua
@@ -0,0 +1,12 @@
+return {
+	metadataFieldsForPhotos = {
+		{
+			id = 'adjustedLuminance',
+			title = LOC "$$$/VideoRenderer/MetadataFieldAdjustedLuminance=Adjusted Luminance",
+			dataType = 'string',
+			browsable = true,
+			searchable = true,
+		},
+	},
+	schemaVersion = 1,
+}
